4a80d19b40a5667047c8ed09c6f30aea4171c685
Davi Mateus <davi.2020109493@unicap.br>
2023-04-05 16:10:19 -0300

    feat: finish add to basket and remove from basket

--- src/components/CheckoutProduct.tsx
+++ src/components/CheckoutProduct.tsx
@@ -1,10 +1,34 @@
 import { StarIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+
+import { addToBasket, removeFromBasket } from '../slices/basketSlice';
 
 import { ProductProps } from '@/types';
 
 export const CheckoutProduct = ({ product }: { product: ProductProps }): JSX.Element => {
+  const dispatch = useDispatch();
+
+  const addItemToBasket = (): void => {
+    const productToAdd = {
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      description: product.description,
+      category: product.category,
+      image: product.image,
+      rating: product.rating,
+      hasPrime: product.hasPrime,
+    };
+
+    dispatch(addToBasket(productToAdd));
+  };
+
+  const removeItemFromBasket = (): void => {
+    dispatch(removeFromBasket({ id: product.id }));
+  };
+
   return (
     <div className='grid grid-cols-5'>
       <Image src={product.image} height={200} width={200} objectFit='contain' alt='image' />
@@ -37,8 +61,12 @@ export const CheckoutProduct = ({ product }: { product: ProductProps }): JSX.Ele
       </div>
 
       <div className='flex flex-col space-y-2 my-auto justify-self-end'>
-        <button className='button'>Add to Basket</button>
-        <button className='button'>Remove from Basket</button>
+        <button className='button' onClick={addItemToBasket}>
+          Add to Basket
+        </button>
+        <button className='button' onClick={removeItemFromBasket}>
+          Remove from Basket
+        </button>
       </div>
     </div>
   );
--- src/slices/basketSlice.ts
+++ src/slices/basketSlice.ts
@@ -12,7 +12,19 @@ export const basketSlice = createSlice({
     addToBasket: (state: any, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex((basketItem: any) => basketItem.id === action.payload.id);
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Can't remove product (id: ${action.payload.id}) as it's not in basket!`);
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
