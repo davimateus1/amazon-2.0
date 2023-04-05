c4a0203a0266f1f7e6ed9dce256695a686e42bb3
Davi Mateus <davi.2020109493@unicap.br>
2023-04-05 13:46:01 -0300

    feat: left side checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { Header } from '../components';
 
 const Checkout = (): JSX.Element => {
@@ -5,7 +7,21 @@ const Checkout = (): JSX.Element => {
     <div className='bg-gray-100'>
       <Header />
 
-      <h1>Checkout PAGE</h1>
+      <main className='lg:flex max-w-screen-2xl mx-auto'>
+        <div className='flex-grow m-5 shadow-sm'>
+          <Image
+            src='https://links.papareact.com/ikj'
+            alt='banner'
+            width={1020}
+            height={250}
+            objectFit='contain'
+          />
+        </div>
+
+        <div className='flex flex-col p-5 space-y-10 bg-white'>
+          <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+        </div>
+      </main>
     </div>
   );
 };
