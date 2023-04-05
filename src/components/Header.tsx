fe72441a3684ecc529435592dabcb8b82164e222
Davi Mateus <davi.2020109493@unicap.br>
2023-04-04 22:23:57 -0300

    feat: finish bottom nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 
 export const Header = (): JSX.Element => {
@@ -44,7 +44,22 @@ export const Header = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div></div>
+
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+        <p className='link flex items-center'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today`s Deals</p>
+        <p className='hidden lg:inline-flex link'>Electronics</p>
+        <p className='hidden lg:inline-flex link'>Food & Grocery</p>
+        <p className='hidden lg:inline-flex link'>Prime</p>
+        <p className='hidden lg:inline-flex link'>Buy Again</p>
+        <p className='hidden lg:inline-flex link'>Shopper Toolkit</p>
+        <p className='hidden lg:inline-flex link'>Health & Personal Care</p>
+      </div>
     </header>
   );
 };
