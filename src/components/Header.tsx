8591011653e0f07dac4e199f7ac056a85c7ba694
Davi Mateus <davi.2020109493@unicap.br>
2023-04-04 22:16:26 -0300

    feat: finish top nav section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 
 export const Header = (): JSX.Element => {
@@ -22,6 +22,26 @@ export const Header = (): JSX.Element => {
             type='text'
           />
           <SearchIcon className='h-12 p-4' />
+        </div>
+
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hello, Davi Mateus</p>
+            <p className='font-extrabold md:text-sm'>Account & Lists</p>
+          </div>
+
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+
+          <div className='relative link flex items-center'>
+            <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+              8
+            </span>
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
+          </div>
         </div>
       </div>
       <div></div>
