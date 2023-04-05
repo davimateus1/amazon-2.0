df6ddc49f90dfcc65c127a23bd433743ca2d723a
Davi Mateus <davi.2020109493@unicap.br>
2023-04-05 10:21:31 -0300

    feat: start next auth

---
+++ src/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
